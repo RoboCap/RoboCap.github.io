59b333efeb081742014a09a14b813b1a937f0a32
Lukasz Laszkiewicz <lukasz.laszkiewicz@unic.com>
2017-05-30 13:00:08 +0200

    popup for events and description for sp12.2017.05.29

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$('#fullpage').fullpage( {	
-	
-	sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
-				responsive: 900,
-				anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy','kontakt'],
-				menu: '#menu',
-				autoScrolling: false,
+    $('#fullpage').fullpage({
+
+        sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
+        responsive: 900,
+        anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy', 'kontakt'],
+        menu: '#menu',
+        scrollOverflow: true,
         fitToSection: false,
-
-	});
+    });
 
     /* ======= Fixed header when scrolled ======= */
     $('#header').addClass('navbar-fixed-top');
-        
-	$('div.facebook').mouseenter(function(){
-		$(this).stop(true, false).animate({"left": "0"}, 400);	
-	});
-	$('div.facebook').mouseleave(function(){
-		$(this).stop(true, false).animate({"left": "-462px"}, 400);
-	});
+
+    $('div.facebook').mouseenter(function () {
+        $(this).stop(true, false).animate({"left": "0"}, 400);
+    });
+    $('div.facebook').mouseleave(function () {
+        $(this).stop(true, false).animate({"left": "-462px"}, 400);
+    });
+
+    /* ======= magnificPopup ======= */
+    $('.ajax-popup-link').magnificPopup({
+        closeOnContentClick: true,
+        type: 'ajax',
+        ajax: {
+            settings: null,
+            cursor: 'mfp-ajax-cur', // CSS class that will be added to body during the loading (adds "progress" cursor)
+            tError: '<a href="%url%">The content</a> could not be loaded.' //  Error message, can contain %curr% and %total% tags if gallery is enabled
+        }
+    });
 });
 	
 
