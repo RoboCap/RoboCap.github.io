2cc4f387821ccaae3f6668042f9a6f8be3f53c27
Lukasz Laszkiewicz <lukasz.laszkiewicz@unic.com>
2017-06-01 14:40:19 +0200

    work in progress, magnific-popup doesn't initialise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,49 @@
 $(document).ready(function () {
 
-    var fpInitialize = function() {
-        $('#fullpage').fullpage({
+    var newEventsLoaded = false;
 
+    $('#fullpage').fullpage({
             sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
             responsive: 900,
             anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy', 'kontakt'],
             menu: '#menu',
             autoScrolling: false,
             fitToSection: false,
-        });
-    }
+            afterLoad: function(anchorLink){
+                if(anchorLink == 'wydarzenia') {
+                    if (!newEventsLoaded) {
+                        $.ajax({
+                            url: "data/wydarzenia.json"
+                        })
+                        .success(function (response) {
+                            $('#wydarzenia-nowe').innerHTML = '';
+                            $.each(response, function(index, wydarzenie) {
+                                wydarzenie.id = 'wydarzenie' + index;
 
-    fpInitialize();
+                                $('#wydarzenia-nowe').append(ich.wydarzenieSzczegolyTemplate(wydarzenie));
+                                $('#wydarzenia-nowe').append(ich.wydarzenieTemplate(wydarzenie));
+
+                                $('link' + wydarzenie.id).magnificPopup({
+                                    type: 'inline',
+                                    callbacks: {
+                                        beforeOpen: function() {
+                                            $('#header').removeClass('navbar-fixed-top');
+                                        },
+                                        afterClose: function() {
+                                            document.documentElement.style.overflow="visible";
+                                            $('#header').addClass('navbar-fixed-top');
+                                        }
+                                    }
+                                });
+                            });
+
+                            //$.fn.fullpage.reBuild();
+                            newEventsLoaded = true;
+                        });
+                    }
+                }
+            }
+        });
 
     /* ======= Fixed header when scrolled ======= */
     $('#header').addClass('navbar-fixed-top');
@@ -22,26 +53,6 @@ $(document).ready(function () {
     });
     $('div.facebook').mouseleave(function () {
         $(this).stop(true, false).animate({"left": "-462px"}, 400);
-    });
-
-    /* ======= magnificPopup ======= */
-    $('.ajax-popup-link').magnificPopup({
-        closeOnContentClick: true,
-        type: 'ajax',
-        ajax: {
-            settings: null,
-            cursor: 'mfp-ajax-cur', // CSS class that will be added to body during the loading (adds "progress" cursor)
-            tError: '<a href="%url%">The content</a> could not be loaded.' //  Error message, can contain %curr% and %total% tags if gallery is enabled
-        },
-        callbacks : {
-            beforeOpen: function() {
-                $('#header').removeClass('navbar-fixed-top');
-            },
-            afterClose: function() {
-                document.documentElement.style.overflow="visible";
-                $('#header').addClass('navbar-fixed-top');
-            }
-        }
     });
 });
 	
