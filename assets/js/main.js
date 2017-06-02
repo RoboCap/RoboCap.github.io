790337342086b24d48e57237d2b81016df685deb
Lukasz Laszkiewicz <lukasz.laszkiewicz@unic.com>
2017-06-02 10:04:00 +0200

    externalizing content for events popup pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,36 @@ $(document).ready(function () {
 
     var newEventsLoaded = false;
 
+    function renderWydarzenie(wydarzenie, index) {
+        var sekcja = (index > 5 ? '#wydarzenia-wszystkie' : '#wydarzenia-nowe')
+
+        wydarzenie.id = 'wydarzenie' + index;
+
+        $(sekcja).append(ich.wydarzenieSzczegolyTemplate(wydarzenie));
+        $(sekcja).append(ich.wydarzenieTemplate(wydarzenie));
+
+        $('#link' + wydarzenie.id).magnificPopup({
+            type: 'inline',
+            closeOnContentClick: true,
+            callbacks: {
+                beforeOpen: function () {
+                    $('#header').removeClass('navbar-fixed-top');
+                },
+                afterClose: function () {
+                    document.documentElement.style.overflow = "visible";
+                    $('#header').addClass('navbar-fixed-top');
+                }
+            }
+        });
+    }
+
     $('#fullpage').fullpage({
-            sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
-            responsive: 900,
-            anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy', 'kontakt'],
-            menu: '#menu',
-            autoScrolling: false,
-            fitToSection: false,
+        sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
+        responsive: 900,
+        anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy', 'kontakt'],
+        menu: '#menu',
+        autoScrolling: false,
+        fitToSection: false,
             afterLoad: function(anchorLink){
                 if(anchorLink == 'wydarzenia') {
                     if (!newEventsLoaded) {
@@ -17,28 +40,17 @@ $(document).ready(function () {
                         })
                         .success(function (response) {
                             $('#wydarzenia-nowe').innerHTML = '';
+                            $('#wydarzenia-wszystkie').innerHTML = '';
+
                             $.each(response, function(index, wydarzenie) {
-                                wydarzenie.id = 'wydarzenie' + index;
-
-                                $('#wydarzenia-nowe').append(ich.wydarzenieSzczegolyTemplate(wydarzenie));
-                                $('#wydarzenia-nowe').append(ich.wydarzenieTemplate(wydarzenie));
-
-                                $('link' + wydarzenie.id).magnificPopup({
-                                    type: 'inline',
-                                    callbacks: {
-                                        beforeOpen: function() {
-                                            $('#header').removeClass('navbar-fixed-top');
-                                        },
-                                        afterClose: function() {
-                                            document.documentElement.style.overflow="visible";
-                                            $('#header').addClass('navbar-fixed-top');
-                                        }
-                                    }
-                                });
+                                renderWydarzenie(wydarzenie, index);
                             });
 
-                            //$.fn.fullpage.reBuild();
+                            $.fn.fullpage.reBuild();
                             newEventsLoaded = true;
+                        })
+                        .error(function(jqXHR, textStatus, errorThrown){
+                            console.error(errorThrown);
                         });
                     }
                 }
