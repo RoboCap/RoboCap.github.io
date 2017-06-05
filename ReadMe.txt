8da3f9bd69bb91aa12eaa9385bd45d92ce434a9e
Łukasz Łaszkiewicz <bushowie@gmail.com>
2017-06-05 08:44:59 +0200

    Feature/wydarzenia (#28)
    * work in progress, magnific-popup doesn't initialise
    
    * externalizing content for events popup pages
    
    * documentation and user manual

--- ReadMe.txt
+++
@@ -1,7 +0,0 @@
-Template bases on: http://www.bootstrapzero.com/bootstrap-template/devaid
-
-
-License: 
-=======================================================================
-This template is free under the Creative Commons Attribution 3.0 License.
-https://creativecommons.org/licenses/by/3.0/
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,61 @@
 $(document).ready(function () {
 
-    var fpInitialize = function() {
-        $('#fullpage').fullpage({
+    var newEventsLoaded = false;
 
-            sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
-            responsive: 900,
-            anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy', 'kontakt'],
-            menu: '#menu',
-            autoScrolling: false,
-            fitToSection: false,
+    function renderWydarzenie(wydarzenie, index) {
+        var sekcja = (index > 5 ? '#wydarzenia-wszystkie' : '#wydarzenia-nowe')
+
+        wydarzenie.id = 'wydarzenie' + index;
+
+        $(sekcja).append(ich.wydarzenieSzczegolyTemplate(wydarzenie));
+        $(sekcja).append(ich.wydarzenieTemplate(wydarzenie));
+
+        $('#link' + wydarzenie.id).magnificPopup({
+            type: 'inline',
+            closeOnContentClick: true,
+            callbacks: {
+                beforeOpen: function () {
+                    $('#header').removeClass('navbar-fixed-top');
+                },
+                afterClose: function () {
+                    document.documentElement.style.overflow = "visible";
+                    $('#header').addClass('navbar-fixed-top');
+                }
+            }
         });
     }
 
-    fpInitialize();
+    $('#fullpage').fullpage({
+        sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#fffff', '#17baef'],
+        responsive: 900,
+        anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'partnerzy', 'kontakt'],
+        menu: '#menu',
+        autoScrolling: false,
+        fitToSection: false,
+            afterLoad: function(anchorLink){
+                if(anchorLink == 'wydarzenia') {
+                    if (!newEventsLoaded) {
+                        $.ajax({
+                            url: "data/wydarzenia.json"
+                        })
+                        .success(function (response) {
+                            $('#wydarzenia-nowe').innerHTML = '';
+                            $('#wydarzenia-wszystkie').innerHTML = '';
+
+                            $.each(response, function(index, wydarzenie) {
+                                renderWydarzenie(wydarzenie, index);
+                            });
+
+                            $.fn.fullpage.reBuild();
+                            newEventsLoaded = true;
+                        })
+                        .error(function(jqXHR, textStatus, errorThrown){
+                            console.error(errorThrown);
+                        });
+                    }
+                }
+            }
+        });
 
     /* ======= Fixed header when scrolled ======= */
     $('#header').addClass('navbar-fixed-top');
@@ -22,26 +65,6 @@ $(document).ready(function () {
     });
     $('div.facebook').mouseleave(function () {
         $(this).stop(true, false).animate({"left": "-462px"}, 400);
-    });
-
-    /* ======= magnificPopup ======= */
-    $('.ajax-popup-link').magnificPopup({
-        closeOnContentClick: true,
-        type: 'ajax',
-        ajax: {
-            settings: null,
-            cursor: 'mfp-ajax-cur', // CSS class that will be added to body during the loading (adds "progress" cursor)
-            tError: '<a href="%url%">The content</a> could not be loaded.' //  Error message, can contain %curr% and %total% tags if gallery is enabled
-        },
-        callbacks : {
-            beforeOpen: function() {
-                $('#header').removeClass('navbar-fixed-top');
-            },
-            afterClose: function() {
-                document.documentElement.style.overflow="visible";
-                $('#header').addClass('navbar-fixed-top');
-            }
-        }
     });
 });
 	
